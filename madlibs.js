e3fe02610dbab69c006c84a649dd43a76a641621
Sandesh Chinchole <31442991+SandeshChinchole@users.noreply.github.com>
2021-01-02 21:45:07 -0500

    uploaded madlibs game

---
+++ madlibs.js
@@ -0,0 +1,6 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = "I had a " + myNoun + " who was " + myAdjective + " and he " + myVerb + " after the squirrels " + myAdverb + ".";
